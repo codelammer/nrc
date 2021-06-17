c05f4b85173a56f30b59873659323605791f3a86
codelammer <briancheruiyott@gmail.com>
2021-06-18 01:16:37 +0300

    added the server in order to host in heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const app = express();
+// const PORT = 3000 || process.env.PORT;
+const path = require('path');
+
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+//uncomment before save to server
+
+app.listen(process.env.PORT, ()=>{
+});
